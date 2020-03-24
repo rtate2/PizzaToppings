157fce9862639b433690ab425cdf53f4c3524884
Randy Tate <randytatejr@gmail.com>
2020-03-23 21:20:41 -0500

    Add project files.

---
+++ PizzaToppings/Pizza.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaToppings
+{
+    class Pizza
+    {
+        public List<string> Toppings { get; set; }
+    }
+}
---
+++ PizzaToppings/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PizzaToppings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pizzas = JsonConvert.DeserializeObject<List<Pizza>>(File.ReadAllText(@"./pizzas.json"));
+
+            //need to compare the arrays inside of the pizza to one another
+            var toppingLists = pizzas.Select(p => string.Join(",", p.Toppings.OrderBy(t => t)));
+
+            //getting distinct topping combinations
+           // var distinctToppingCombinations = toppingLists.Distinct();
+
+            var countOfCombinations = new Dictionary<string, int>();
+
+            foreach (var combination in toppingLists)
+            {
+                if (!countOfCombinations.ContainsKey(combination))
+                {
+                    countOfCombinations.Add(combination, 1);
+                }
+                else
+                {
+                    countOfCombinations[combination] += 1;
+                }
+            }
+
+            var mostOrdered = countOfCombinations.OrderByDescending(item => item.Value).Take(20);
+
+            foreach (var (combination, count) in mostOrdered)
+            {
+                Console.WriteLine($"The topping combination of {combination} was ordered {count} times.");
+            }
+        }
+    }
+}
